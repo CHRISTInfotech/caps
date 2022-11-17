dd96e120d6e5075a868374010b6a31c65f824046
alwin-97 <alwinjosephkaduppil@gmail.com>
2022-11-17 16:57:31 +0530

    updates

--- CAPS/settings.py
+++ CAPS/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dkx=l(n(n^+(gq-3-ot=39_t-u_gx=@k*9)514azsk=^=ffjgr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG == False:
     ALLOWED_HOSTS = ['10.5.5.32']
