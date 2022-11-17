d1b4b434d1bbc3b6508213d4bfdb0396d806d321
alwin-97 <alwinjosephkaduppil@gmail.com>
2022-11-17 16:39:52 +0530

    file update static

--- CAPS/settings.py
+++ CAPS/settings.py
@@ -145,10 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 if DEBUG == False:
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
     STATIC_ROOT = '/home/administrator/caps/static'
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static/')
+        os.path.join(BASE_DIR, 'static')
     ]
 
     MEDIA_URL = '/media/'
